b5d9a07b614d881ef5c942f32e32cd4c4f9b29e0
Saskia Kriibi <90192374+saskia002@users.noreply.github.com>
2024-03-28 08:56:07 +0200

    3. KT

--- app/(tabs)/vibration.tsx
+++ app/(tabs)/vibration.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
 import { Vibration } from "react-native";
