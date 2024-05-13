387076583f3e8922642865b2cd026772e0bf5fa4
Saskia Kriibi <90192374+saskia002@users.noreply.github.com>
2024-05-13 20:12:18 +0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, View, StatusBar } from "react-native";
 import * as Location from "expo-location";
 import BeachesAccordion from "@/components/BeachesAccordion";
 import { Surface, Text } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const beaches = [
 	{
@@ -156,124 +157,104 @@ export default function App() {
 	};
 
 	return (
-		<View
-			style={{
-				flex: 1,
-				marginTop: StatusBar.currentHeight ?? 24 + 2,
-			}}
-		>
-			{location && weather && (
-				<View style={styles.container}>
-					<MapView
-						ref={mapRef}
-						showsCompass
-						showsScale
-						showsUserLocation
-						loadingEnabled
-						initialRegion={{
-							latitude: location?.latitude,
-							longitude: location?.longitude,
-							latitudeDelta: 1,
-							longitudeDelta: 1,
-						}}
-						style={styles.map}
-						onRegionChangeComplete={(region) => {
-							updateCords(region.longitude, region.latitude);
-						}}
-					>
-						{beaches.map((beach) => (
-							<Marker
-								key={beach.id}
-								coordinate={{
-									latitude: parseFloat(beach.latitude),
-									longitude: parseFloat(beach.longitude),
-								}}
-								title={beach.name} // Marker title
-							/>
-						))}
-					</MapView>
-					{weather?.current && weather?.daily && (
-						<View
-							style={{
-								display: "flex",
-								height: "100%",
-								flexDirection: "column",
-								justifyContent: "space-between",
+		<SafeAreaView style={{ ...StyleSheet.absoluteFillObject }}>
+			<View
+				style={{
+					flex: 1,
+				}}
+			>
+				{location && weather && (
+					<View>
+						<MapView
+							ref={mapRef}
+							showsUserLocation
+							loadingEnabled
+							initialRegion={{
+								latitude: location?.latitude,
+								longitude: location?.longitude,
+								latitudeDelta: 1,
+								longitudeDelta: 1,
+							}}
+							style={styles.map}
+							onRegionChangeComplete={(region) => {
+								updateCords(region.longitude, region.latitude);
 							}}
 						>
-							<Surface
+							{beaches.map((beach) => (
+								<Marker
+									key={beach.id}
+									coordinate={{
+										latitude: parseFloat(beach.latitude),
+										longitude: parseFloat(beach.longitude),
+									}}
+									title={beach.name} // Marker title
+								/>
+							))}
+						</MapView>
+						{weather?.current && weather?.daily && (
+							<View
 								style={{
 									display: "flex",
-									flexDirection: "row",
+									height: "100%",
+									flexDirection: "column",
+									justifyContent: "space-between",
 								}}
-								mode="flat"
-								elevation={1}
 							>
-								<View>
-									<BeachesAccordion
-										beaches={beaches}
-										onBeachSelect={handleBeachSelect}
-										handleAccordionPress={handleCloseAccordion}
-										isOpen={isAccordionOpen}
-									/>
-								</View>
-								<View
+								<Surface
 									style={{
 										display: "flex",
-										flexDirection: "column",
-										justifyContent: "flex-start",
-										alignItems: "flex-start",
-										width: "auto",
-										paddingLeft: 12,
-										paddingTop: 4,
+										flexDirection: "row",
 									}}
+									mode="flat"
+									elevation={1}
 								>
-									{calcBeachDay() ? <Text>Perfect day for beach!</Text> : <Text>Not a beach day!</Text>}
-									{calcBBQDay() ? <Text>Perfect day for BBQ!</Text> : <Text>Not a BBQ day!</Text>}
-									{!calcBeachDay() && !calcBBQDay() && <Text>A sad miserable day :(</Text>}
-									{calcBeachDay() && calcBBQDay() && <Text>Absolutely perfect day :)</Text>}
-								</View>
-							</Surface>
+									<View>
+										<BeachesAccordion
+											beaches={beaches}
+											onBeachSelect={handleBeachSelect}
+											handleAccordionPress={handleCloseAccordion}
+											isOpen={isAccordionOpen}
+										/>
+									</View>
+									<View
+										style={{
+											display: "flex",
+											flexDirection: "column",
+											justifyContent: "flex-start",
+											alignItems: "flex-start",
+											width: "auto",
+											paddingLeft: 12,
+											paddingTop: 4,
+										}}
+									>
+										{calcBeachDay() ? <Text>Perfect day for beach!</Text> : <Text>Not a beach day!</Text>}
+										{calcBBQDay() ? <Text>Perfect day for BBQ!</Text> : <Text>Not a BBQ day!</Text>}
+										{!calcBeachDay() && !calcBBQDay() && <Text>A sad miserable day :(</Text>}
+										{calcBeachDay() && calcBBQDay() && <Text>Absolutely perfect day :)</Text>}
+									</View>
+								</Surface>
 
-							<Surface mode="flat" elevation={1} style={styles.mapTextBox}>
-								<Text>Temp: {weather?.current.temperature_2m} °C</Text>
-								<Text>Feels like: {weather?.current.temperature_2m} °C</Text>
-								<Text>Rain: {weather?.current.rain} mm</Text>
-								<Text>Rain prob: {weather?.daily.precipitation_probability_mean} %</Text>
-								<Text>Pres: {weather?.current.pressure_msl} hPa</Text>
-							</Surface>
-						</View>
-					)}
-				</View>
-			)}
-		</View>
+								<Surface mode="flat" elevation={1} style={styles.mapTextBox}>
+									<Text>Temp: {weather?.current.temperature_2m} °C</Text>
+									<Text>Feels like: {weather?.current.temperature_2m} °C</Text>
+									<Text>Rain: {weather?.current.rain} mm</Text>
+									<Text>Rain prob: {weather?.daily.precipitation_probability_mean} %</Text>
+									<Text>Pres: {weather?.current.pressure_msl} hPa</Text>
+								</Surface>
+							</View>
+						)}
+					</View>
+				)}
+			</View>
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		...StyleSheet.absoluteFillObject,
-	},
-	mapText: {
-		justifyContent: "flex-end",
-		...StyleSheet.absoluteFillObject,
-	},
 	mapTextBox: {
 		display: "flex",
 		flexDirection: "column",
 		width: 128,
-		margin: 0,
-		padding: 8,
-	},
-	epic: {
-		...StyleSheet.absoluteFillObject,
-		alignItems: "center",
-	},
-	epicInner: {
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		width: 180,
 		margin: 0,
 		padding: 8,
 	},
